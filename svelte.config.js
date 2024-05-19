59acbf0309e9aa91021bebb22da4a9841d775bcd
aditya-xq <adityavivek1998@gmail.com>
2024-05-20 01:55:39 +0530

    Setting up github pages deployment

--- svelte.config.js
+++ svelte.config.js
@@ -2,14 +2,9 @@ import adapter from '@sveltejs/adapter-static';
 
 export default {
 	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
+		adapter: adapter(),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/<REPO_NAME>' : ''
+		},
 	}
 };
