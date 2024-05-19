533c7c42fe9187fd12f65024c702a892b619debf
aditya-xq <adityavivek1998@gmail.com>
2024-05-20 02:19:11 +0530

    deployment fix

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,11 @@ import adapter from '@sveltejs/adapter-static';
 
 export default {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		}),
 		paths: {
 			base: process.env.NODE_ENV === 'production' ? '/the-research-nest.github.io' : ''
 		},
